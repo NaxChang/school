3421d20e5d8bb933ce1b7deb2b4b252cefbcb998
naxchang <naxchang@gmail.com>
2024-08-03 08:40:45 +0800

    0803

--- lesson0727_1/2024_08_03/2024_08_03.py
+++ lesson0727_1/2024_08_03/2024_08_03.py
@@ -3,6 +3,8 @@ import os
 from datetime import datetime
 import random
 
+if __name__ == '__main__':
+
 current_path = os.path.abspath(__name__) #取得目前檔案路徑
 directory_name = os.path.dirname(current_path) #取得目前資料夾路行
 data_path = os.path.join(directory_name,'data') #目前資料夾路徑加上data目錄
