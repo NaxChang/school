0d6741c05fcca3b08f320190d77dd0a135d35e30
naxchang <naxchang@gmail.com>
2024-08-03 13:14:30 +0800

    "0803_13:15"

--- data/2024_08_03_1.py
+++ data/2024_08_03_1.py
@@ -1,17 +1,13 @@
 from datetime import datetime
-from tools.file import created_log_file,record_info
-
-
+from tools.file import created_log_file, record_info
 
 
 def main():
     now = datetime.now()
-    current_file_name = now.strftime('%Y_%m_%d.log')
+    current_file_name = now.strftime("%Y_%m_%d.log")
     log_path = created_log_file(current_file_name)
     record_info(log_path)
-    
-    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
