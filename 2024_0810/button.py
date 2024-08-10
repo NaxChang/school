132624836b00544cd6f3d38ce89877a872cf2cb3
naxchang <naxchang@gmail.com>
2024-08-10 10:52:53 +0800

    11:00

---
+++ 2024_0810/button.py
@@ -0,0 +1,9 @@
+from gpiozero import Button
+
+button = Button(pin=18)
+
+while True:
+    if button.is_pressed:
+        print("The button was pressed!")
+    else:
+        print("The button was not pressed!")
