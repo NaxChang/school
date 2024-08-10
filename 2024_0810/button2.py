f55416195f773b9fce2de0caaa4c8c975964a5ac
naxchang <naxchang@gmail.com>
2024-08-10 14:15:52 +0800

    Led 開和關

---
+++ 2024_0810/button2.py
@@ -0,0 +1,19 @@
+import signal
+from gpiozero import Button, LED
+
+
+def user_release():
+    print("使用者按下放開")
+    led.toggle()  # 按一次會進行關或開
+    if led.is_lit:
+        print("light on")
+    else:
+        print("light off")
+
+
+if __name__ == "__main__":  # button 全域
+    button = Button(pin=18)
+    # print(button.pin)
+    button.when_released = user_release
+    led = LED(pin=25)
+    signal.pause()
