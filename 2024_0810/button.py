2913666e48d0a1b0aa824008558ec312f2faa25d
naxchang <naxchang@gmail.com>
2024-08-13 18:59:33 +0800

    '19:00'

--- 2024_0810/button.py
+++ 2024_0810/button.py
@@ -5,6 +5,6 @@ from gpiozero import Button
 button = Button(pin=18)
 while True:
     if button.is_pressed:
-        print("The button was pressed!")
+        print("按鈕被案了")
     else:
-        print("The button was not pressed!")
+        print("按鈕沒被案了")
--- TQC_python程式語言第二版/405/405.py
+++ TQC_python程式語言第二版/405/405.py
@@ -1,2 +1,9 @@
 L = [int(input()) for i in range(3)]
-x = 1
+
+with open("read.txt", "r", encoding="utf8") as f:
+    data = f.read().split()
+
+for i in range(3):
+    s1 = str(L[i]) * L[i]
+    s2 = data[i][L[i] :]
+    print(s1 + s2)
