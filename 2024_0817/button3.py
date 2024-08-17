683843b5cff6225a9b8b245a812ba22609817a9a
naxchang <naxchang@gmail.com>
2024-08-17 15:15:38 +0800

    1515

--- 2024_0817/button3.py
+++ 2024_0817/button3.py
@@ -5,38 +5,35 @@ import paho.mqtt.publish as publish
 
 
 def user_release():
-    print("User presses and releases")
-    led.toggle()  # 按一次會進行關或開
+    print("使用者按下放開")
+    led.toggle()
     now = datetime.now()
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     print(now_str)
     if led.is_lit:
         message = f"""{{
-            "status": true,
-            "date": {now_str},
-            "topic": "501教室/學員nameless",
+            "status":true,
+            "date":{now_str},
+            "topic":"501教室/老師桌燈"
         }}"""
-
-        # message = "燈是開的"
         print(message)
         publish.single(
-            topic="501教室/學員nameless", payload=message, hostname="127.0.0.1", qos=2
+            topic="501教室/老師桌燈", payload=message, hostname="127.0.0.1", qos=2
         )
     else:
         message = f"""{{
-            "status": false,
-            "date": {now_str},
-            "topic": "501教室/學員nameless",
+            "status":false,
+            "date":{now_str},
+            "topic":"501教室/老師桌燈"
         }}"""
         print(message)
         publish.single(
-            topic="501教室/學員nameless", payload=message, hostname="127.0.0.1", qos=2
+            topic="501教室/老師桌燈", payload=message, hostname="127.0.0.1", qos=2
         )
 
 
-if __name__ == "__main__":  # button 全域
+if __name__ == "__main__":
     button = Button(pin=18)
-    # print(button.pin)
     button.when_released = user_release
     led = LED(pin=25)
     signal.pause()
--- 2024_0817/receive.py
+++ 2024_0817/receive.py
@@ -2,31 +2,20 @@ import paho.mqtt.client as mqtt
 import redis
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-redis_conn = redis.Redis(
-    host=os.environ['REDIS_HOST'],
-    port=6379,
-    password="password",
-)
-
-
-# print(redis_conn.ping())
-# 會傳出redis的實體,實體內有屬性,屬性有兩種attribute,propertty
-# 在Linux內密碼不讓別人看到,要設定環境變數
+redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port=6379,password=os.environ['REDIS_PASSWORD'])
 
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
-    message = msg.payload.decode("utf8")
-    redis_conn.rpush(topic, message)
-    print(f"top={topic},message:{message}")
+    message = msg.payload.decode('utf-8')
+    redis_conn.rpush(topic,message)
+    print(f"topic={topic},message:{message}")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_message = on_message
     client.connect(os.environ['MQTT_SERVER'])
-    client.subscribe("501教室/學員nameless", qos=2)
+    client.subscribe('501教室/老師桌燈',qos=2)
     client.loop_forever()
