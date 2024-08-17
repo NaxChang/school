91b8f4c0e66a90e9972ceb3ec68c17ec4c7cb7f0
naxchang <naxchang@gmail.com>
2024-08-17 13:55:00 +0800

    0817 14:00

--- 2024_0817/receive.py
+++ 2024_0817/receive.py
@@ -1,10 +1,22 @@
 import paho.mqtt.client as mqtt
+import redis
+
+redis_conn = redis.Redis(
+    host="localhost",
+    port=6379,
+    password="password",
+)
+
+
+# print(redis_conn.ping())
+# 會傳出redis的實體,實體內有屬性,屬性有兩種attribute,propertty
 
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
-    message = msg.payload.decode('utf8')
-    print(f'top={topic},message:{message}')
+    message = msg.payload.decode("utf8")
+    redis_conn.rpush(topic, message)
+    print(f"top={topic},message:{message}")
 
 
 if __name__ == "__main__":
