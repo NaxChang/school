14fef3c75599ccbfe0e1ccf03270ff9c4d6071c7
naxchang <naxchang@gmail.com>
2024-08-29 20:31:27 +0800

    0829 20:00

--- TQC_python程式語言第二版/406.py
+++ TQC_python程式語言第二版/406.py
@@ -15,25 +15,6 @@ def trans(s):
                 ss += s1
     return ss
 
-s=input()
+
+s = input()
 print(trans(s))
-
-# #4-6
-# def trans(s):
-#   keys='qwertyuiopasdfghjklzxcvbnm'
-
-#   ss=''
-#   for x in s:
-#     if x=='p' or x=='P' or x=='L' or x=='l' or x=='M' or x=='m':
-#       ss+=x
-#     else:
-#       if x.isupper():
-#         x = x.lower()
-#         s1 = keys[keys.index(x)+1]
-#         ss+=s1.upper()
-#       else:
-#         s1 = keys[keys.index(x)+1]
-#         ss+=s1
-#   return ss
-# s=input()
-# print(trans(s))
--- TQC_python程式語言第二版/409/409.py
+++ TQC_python程式語言第二版/409/409.py
@@ -1,9 +1,9 @@
-with open('read.txt','r',encoding='utf8')as f: #首先讀檔,變數存放
-    data=f.read() #讀出來的檔案也給它變數data存放
+with open("read.txt", "r", encoding="utf8") as f:
+    data = f.read()
 
 newdata = ""
 for x in data:
-    newdata+=(chr( (ord(x)-97+2)%26 +97 ) )
+    newdata += chr((((ord(x) - 97) + 2) % 26) + 97)
 
-with open('write.txt','w',encoding='utf8')as f: # 寫檔
+with open("write.txt", "w", encoding="utf8") as f:
     f.write(newdata)
